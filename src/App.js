b8fe2f5e1e2154426ddafeab37bf2d934b72d3f1
rajkumar <rsrajkumar11@live.com>
2019-01-14 10:50:36 +0530

    commeting first time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import meme from './resources/meame'
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text:meme[0]["quote"],
+      author:meme[0]["author"]
+    }
+    this.handleClick=this.handleClick.bind(this);
+  }
+  
+  handleClick(event){
+    let r=Math.round(Math.random()*12);
+    console.log(r);
+
+    this.setState({
+      text:meme[r]["quote"],
+      author:meme[r]["author"]
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <h1>Random Quote Machine</h1>
+        <div id="quote-box" > 
+          <p id="text"><span>"</span>{ this.state.text }</p>
+          <p id="author">-{ this.state.author }</p>
+          <div class="buttons">
+            <a class="button" id="tweet-quote" title="Tweet this quote!" target="_blank" href='twitter.com/intent/tweet'>
+              <i class="fa fa-twitter"></i>
+            </a>
+            <a class="button" id="tumblr-quote" title="Post this quote on tumblr!" target="_blank">
+              <i class="fa fa-tumblr"></i>
+            </a>
+          </div>
+          <input type="button" id="new-quote" onClick={ this.handleClick } value="New quote" />
+        </div>
       </div>
     );
   }
---
+++ src/resources/meame.js
@@ -0,0 +1,58 @@
+const meme=[
+    {
+      quote:'I have been impressed with the urgency of doing. Knowing is not enough; we must apply. Being willing is not enough; we must do.',
+      author:' Leonardo da Vinci'
+    },
+    {
+      quote:'A truly rich man is one whose children run into his arms when his hands are empty.',
+      author:'Unknown'
+    },
+    {
+      quote:'Remember that not getting what you want is sometimes a wonderful stroke of luck.',
+      author:'Dalai Lama'
+    },
+    {
+      quote:'If you do what you’ve always done, you’ll get what you’ve always gotten.',
+      author:'Tony Robbins'
+    },
+    {
+      quote:'We must balance conspicuous consumption with conscious capitalism.',
+      author:'Kevin Kruse'
+    },
+    {
+      quote:'If you look at what you have in life, you’ll always have more. If you look at what you don’t have in life, you’ll never have enough.',
+      author:'Oprah Winfrey'
+    },
+    {
+      quote:'The best time to plant a tree was 20 years ago. The second best time is now.',
+      author:'Chinese Proverb'
+    },
+    {
+      quote:'We must balance conspicuous consumption with conscious capitalism.',
+      author:'Kevin Kruse'
+    },
+    {
+      quote:'People often say that motivation doesn’t last. Well, neither does bathing. That’s why we recommend it daily.',
+      author:'Zig Ziglar'
+    },
+    {
+      quote:'It’s your place in the world; it’s your life. Go on and do all you can with it, and make it the life you want to live.',
+      author:'Mae Jemison'
+    },
+    {
+      quote:'If you do what you’ve always done, you’ll get what you’ve always gotten.',
+      author:'Tony Robbins'
+    },
+    {
+      quote:'An unexamined life is not worth living.',
+      author:'Socrates'
+  
+    },
+    {
+      quote:'The two most important days in your life are the day you are born and the day you find out why.',
+      author:'Mark Twain'
+    }
+    
+  ];
+
+  export default meme;
